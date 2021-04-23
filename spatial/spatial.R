902d0841f78c0867cdf066e79894d1f7fd41ef3d
Grant McDermott <grant.mcdermott@gmail.com>
2021-04-23 13:46:23 -0700

    Add some extra interactive maps

--- spatial/spatial.R
+++ spatial/spatial.R
@@ -42,3 +42,53 @@ or %>%
 	mapshot(url = here("spatial/oregon_leaflet.html"))
 or %>%
 	mapshot(file = here("spatial/oregon_leaflet.png"))
+
+
+# Lane county leaflet map -------------------------------------------------
+
+lane = 
+	get_acs(
+		geography = "tract", variables = "B25077_001", 
+		state = "OR", county = "Lane County", geometry = TRUE
+	)
+
+lane_pal = colorNumeric(palette = "plasma", domain = lane$estimate)
+
+l =
+	lane %>%
+	mutate(tract = gsub(",.*", "", NAME)) %>% ## Get rid of everything after the first comma
+	st_transform(crs = 4326) %>%
+	leaflet(width = "100%") %>%
+	addProviderTiles(provider = "CartoDB.Positron") %>%
+	addPolygons(
+		# popup = ~tract,
+		popup = ~paste0(tract, "<br>", "Median value: $", prettyNum(estimate, big.mark=",")),
+		stroke = FALSE,
+		smoothFactor = 0,
+		fillOpacity = 0.5,
+		color = ~lane_pal(estimate)
+	) %>%
+	addLegend(
+		"bottomright", 
+		pal = lane_pal, 
+		values = ~estimate,
+		title = "Median home values<br>Lane County, OR",
+		labFormat = labelFormat(prefix = "$"),
+		opacity = 1
+	)
+l %>%
+	mapshot(url = here("spatial/lane_leaflet.html"))
+l %>%
+	mapshot(file = here("spatial/lane_leaflet.png"))
+
+# * mapview version -------------------------------------------------------
+
+# https://github.com/r-spatial/mapview/issues/321
+mapviewOptions(fgb = FALSE)
+l_mv =
+	mapview::mapview(lane, zcol = "estimate", 
+									 layer.name = 'Median home values<br>Lane County, OR')
+l_mv %>%
+	mapshot(url = here("spatial/lane_mapview.html"))
+l_mv %>%
+	mapshot(file = here("spatial/lane_mapview.png"))
